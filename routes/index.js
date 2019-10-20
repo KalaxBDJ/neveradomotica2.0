105ce0fcc3a79ca5435053eba161fcac312f5fee
KalaxBDJ <santiago.salazar9208@unaula.edu.co>
2019-10-19 20:02:35 -0500

    Creating API WEB PAGE

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const measureCtrl = require('../controllers/measure')
+const webCtrl = require('../controllers/pagweb')
 const api = express.Router()
+
 
 
 
@@ -10,5 +12,7 @@ api.post('/mediciones',measureCtrl.saveMeasure)
 api.put('/mediciones/:dataId',measureCtrl.updateMeasure)
 api.delete('/mediciones/:dataId',measureCtrl.deleteMeasure)
 api.get('/create',measureCtrl.fakeMeasure)
+api.get('/principal',webCtrl.getAllMeasures)
+
 
 module.exports = api
--- server/app.js
+++ server/app.js
@@ -5,10 +5,15 @@ const app = express()
 const bodyParser = require('body-parser')
 const api = require('../routes/index')
 
+    
+
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
 app.use('/api',api)
+app.set('views')
+app.set('view engine','ejs')
+
 
 
 module.exports = app
