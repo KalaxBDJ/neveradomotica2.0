ea25ee97d9379954e5d9ef9aed92ce3d9d58dae5
KalaxBDJ <santiago.salazar9208@unaula.edu.co>
2019-10-31 12:21:50 -0500

    Creating views using jquery and ajax

--- public/app.js
+++ public/app.js
@@ -1,20 +1,75 @@
 $(function()
 {
+
+    $('#getTemperatura').on('click',function(){
+
         $.ajax(
-        {
-            url:'/mediciones',
-            type:'GET',
-            success: function(datos)
             {
-                console.log(datos)
-                datos.forEach(dato=>{
-                    console.log(dato.created_at )
-                })
-                
-                
-                
-                
-            }   
-        })
+                url:'/mediciones',
+                type:'GET',
+                success: function(datos)
+                {
+                    let tbody = $('tbody')
+                    tbody.html('')
+                    datos.forEach(dato=>{
+                        if(dato.category=='temperatura')
+                        {
+                            tbody.append(`
+                            <tr>
+                                <td class="created_at">${dato.created_at   }</td>
+                                <td class="value">${dato.value}</td>
+                                <td class="category">${dato.category}</td>
+                                <td class="id">${dato._id}</td>
+                                <td>
+                                    <button class="delete-button">DELETE</button>
+                                </td>
+                            </tr>
+                            `);
+                        }
+                    })
+                    
+                    
+                    
+                    
+                }   
+            })
+
+    })
+
+    $('#getPeso').on('click',function(){
+
+        $.ajax(
+            {
+                url:'/mediciones',
+                type:'GET',
+                success: function(datos)
+                {
+                    let tbody = $('tbody')
+                    tbody.html('')
+                    datos.forEach(dato=>{
+                        if(dato.category=='peso')
+                        {
+                            tbody.append(`
+                            <tr>
+                                <td class="created_at">${dato.created_at   }</td>
+                                <td class="value">${dato.value}</td>
+                                <td class="category">${dato.category}</td>
+                                <td class="id">${dato._id}</td>
+                                <td>
+                                    <button class="delete-button">DELETE</button>
+                                </td>
+                            </tr>
+                            `);
+                        }
+                    })
+                    
+                    
+                    
+                    
+                }   
+            })
+
+    })
+        
     
 })
--- server/app.js
+++ server/app.js
@@ -5,15 +5,17 @@ const app = express()
 const bodyParser = require('body-parser')
 const api = require('../routes/index')
 const path = require('path')
+const morgan = require('morgan')
 
 
+app.use(morgan('combined'))
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname,'../public')))
 
 app.use('/',api)
-app.set('views')
-app.set('view engine','ejs')
+//app.set('views')
+//app.set('view engine','ejs')
 
 
 
