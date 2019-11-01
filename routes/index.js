f1e3190362739e141f48986e9684a127eb12b1b6
KalaxBDJ <santiago.salazar9208@unaula.edu.co>
2019-11-01 14:33:32 -0500

    Error Fixed

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const measureCtrl = require('../controllers/measure')
-const webCtrl = require('../controllers/pagweb')
 const api = express.Router()
 
 
@@ -12,7 +11,6 @@ api.post('/mediciones',measureCtrl.saveMeasure)
 api.put('/mediciones/:dataId',measureCtrl.updateMeasure)
 api.delete('/mediciones/:dataId',measureCtrl.deleteMeasure)
 api.get('/create',measureCtrl.fakeMeasure)
-api.get('/principal',webCtrl.getAllMeasures)
 
 
 module.exports = api
