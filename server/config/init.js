06f0c0c79fa9010560fb98ce568b5633f1baeea7
KalaxBDJ <santiago.salazar9208@unaula.edu.co>
2019-11-04 12:52:41 -0500

    changed db connection string

--- server/config/init.js
+++ server/config/init.js
@@ -1,4 +1,4 @@
 process.env.PORT = process.env.PORT || 3001
-process.env.MONGODB_URL = process.env.MONGODB_URL || 'mongodb+srv://admin:0000@cluster-6mz8v.mongodb.net/test?retryWrites=true&w=majority'
+process.env.MONGODB_URL = process.env.MONGODB_URL || 'mongodb+srv://admin:0000@cluster-6mz8v.mongodb.net/neveradomotica?retryWrites=true&w=majority'
 process.env.FROM_NUMBER = '+12182315058'
 process.env.TO_NUMBER = '+573117858828'
